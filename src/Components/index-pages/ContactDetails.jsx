df79582ba6ca4d3f86f6b0bd6d3873cde39cce87
techsadhisha <tech.sadhisha@gmail.com>
2025-12-30 15:28:58 +0530

    address changed

--- src/Components/index-pages/ContactDetails.jsx
+++ src/Components/index-pages/ContactDetails.jsx
@@ -1,99 +1,119 @@
-import React from 'react';
+import React from "react";
 import {
-    FaPhoneAlt,
-    FaEnvelope,
-    FaMapMarkerAlt,
-    FaFacebookF,
-    FaTwitter,
-    FaInstagram,
-    FaLinkedinIn,
-    FaLocationArrow,
-} from 'react-icons/fa';
+  FaPhoneAlt,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaLocationArrow,
+} from "react-icons/fa";
 
 const ContactDetails = () => {
-    return (
-        <div>
-            <div className="pb-md-4 pb-2">
-                <h2 className="secTitleGreen mb-md-3 text-md-start text-center"> Get In Touch </h2>
-                <p className='paraBlack col-md-10 text-md-start text-center'>
-                    Whether you’re exploring property options, seeking partnership, or have a general enquiry.
-                </p>
-            </div>
-            <div className="row d-flex justify-content-md-start justify-content-center">
-
-                <div className='py-md-2 col-md-5 mb-md-0 mb-3'>
-                    <div className="contact-card">
-                        <h3 className="subTitleGreen d-flex">
-                            <FaPhoneAlt className='me-2' />
-                            <span><h5>Enquire Us</h5></span>
-                        </h3>
-                        <ul>
-                            <p className='paraBlack'>
-                                <span><a className='text-decoration-none paraBlack footer-links' href="+tel: +91 95852 30000"> +91 95852 30000 </a></span> <br />
-
-                            </p>
-                        </ul>
-                    </div>
-
-                </div>
-
-                <div className='py-md-2 col-md-5 '>
-                    <div className="contact-card">
-                        <h3 className="subTitleGreen d-flex">
-                            <FaEnvelope className='me-2' />
-                            <span><h5>Mail Us</h5></span>
-                        </h3>
-                        <ul>
-                            <p className='paraBlack'>
-                                <span><a className='text-decoration-none paraBlack footer-links' href="mailto:sadhishahomes@gmail.com">sadhishahomes@gmail.com</a></span><br />
-
-                            </p>
-                        </ul>
-                    </div>
-
-                </div>
-
-
-            </div>
-
-            <div className="row d-flex justify-content-md-start justify-content-center pt-3 mb-md-5">
-                <div className='py-md-2 col-md-5 mb-md-0 mb-3'>
-                    <div className="contact-card">
-                        <h3 className="subTitleGreen d-flex">
-                            <FaLocationArrow className='me-2' />
-                            <span><h5>Head Office - Puducherry</h5></span>
-                        </h3>
-                        <ul>
-                            <p className='paraBlack'>Sadhisha, <br />
-                                No.36, Pappammal Koil St, Kuruchikuppam, <br />
-                                Puducherry – 605001
-                                India
-                            </p>
-                        </ul>
-                    </div>
-
-                </div>
-
-
-                <div className='py-md-2 col-md-5 '>
-                    <div className="contact-card">
-                        <h3 className="subTitleGreen d-flex">
-                            <FaLocationArrow className='me-2' />
-                            <span><h5>Branch - Chennai</h5></span>
-                        </h3>
-                        <ul>
-                            <p className='paraBlack'>Sadhisha, <br />
-                                Workafella IFL TOWERS Perungudi, OMR<br />
-                                Chennai – 600096
-                                India
-                            </p>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="pb-md-4 pb-2">
+        <h2 className="secTitleGreen mb-md-3 text-md-start text-center">
+          {" "}
+          Get In Touch{" "}
+        </h2>
+        <p className="paraBlack col-md-10 text-md-start text-center">
+          Whether you’re exploring property options, seeking partnership, or
+          have a general enquiry.
+        </p>
+      </div>
+      <div className="row d-flex justify-content-md-start justify-content-center">
+        <div className="py-md-2 col-md-5 mb-md-0 mb-3">
+          <div className="contact-card">
+            <h3 className="subTitleGreen d-flex">
+              <FaPhoneAlt className="me-2" />
+              <span>
+                <h5>Enquire Us</h5>
+              </span>
+            </h3>
+            <ul>
+              <p className="paraBlack">
+                <span>
+                  <a
+                    className="text-decoration-none paraBlack footer-links"
+                    href="+tel: +91 95852 30000"
+                  >
+                    {" "}
+                    +91 95852 30000{" "}
+                  </a>
+                </span>{" "}
+                <br />
+              </p>
+            </ul>
+          </div>
         </div>
-    )
-}
 
-export default ContactDetails
+        <div className="py-md-2 col-md-5 ">
+          <div className="contact-card">
+            <h3 className="subTitleGreen d-flex">
+              <FaEnvelope className="me-2" />
+              <span>
+                <h5>Mail Us</h5>
+              </span>
+            </h3>
+            <ul>
+              <p className="paraBlack">
+                <span>
+                  <a
+                    className="text-decoration-none paraBlack footer-links"
+                    href="mailto:sadhishahomes@gmail.com"
+                  >
+                    sadhishahomes@gmail.com
+                  </a>
+                </span>
+                <br />
+              </p>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="row d-flex justify-content-md-start justify-content-center pt-3 mb-md-5">
+        <div className="py-md-2 col-md-5 mb-md-0 mb-3">
+          <div className="contact-card">
+            <h3 className="subTitleGreen d-flex">
+              <FaLocationArrow className="me-2" />
+              <span>
+                <h5>Head Office - Mumbai</h5>
+              </span>
+            </h3>
+            <ul>
+              <p className="paraBlack">
+                Sadhisha, <br />
+                Anant Enclave, M G Road, <br /> Ville Parle ( East ) <br />{" "}
+                Mumbai - India
+              </p>
+            </ul>
+          </div>
+        </div>
+
+        <div className="py-md-2 col-md-5 ">
+          <div className="contact-card">
+            <h3 className="subTitleGreen d-flex">
+              <FaLocationArrow className="me-2" />
+              <span>
+                <h5>Branch - Chennai</h5>
+              </span>
+            </h3>
+            <ul>
+              <p className="paraBlack">
+                Sadhisha, <br />
+                Workafella IFL TOWERS Perungudi, OMR
+                <br />
+                Chennai – 600096 India
+              </p>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactDetails;
